168b9a87c601950be97a04e5e34f0136930bdd79
m-kiyom <iceplay.fg@mail.ru>
2022-02-28 20:21:38 +0500

    files added

---
+++ src/TEST6.java
@@ -0,0 +1,16 @@
+
+public class TEST6 {
+    public static void main(String[] args) {
+
+        int number = getNumber();
+        System.out.println(number);
+
+    }
+
+    private static int getNumber() {
+        int yak = 50;
+        int du = 45;
+        return yak - du;
+    }
+
+}
